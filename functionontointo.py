561595fa4848914e87b001b396908e42122ec3b5
MUGIWARA102 <likhithchandra3405@gmail.com>
2022-11-07 22:47:57 +0530

    my first push

---
+++ functionontointo.py
@@ -0,0 +1,10 @@
+def strawhats():
+    print("Mugiwara No Luffy")
+    print("Roronoa zoro")
+    print("Nami")
+    print("Ussop")
+    print("Tony Tony Chopper")
+def blackleg():
+    print("Vinsmoke Sanji")
+strawhats()
+blackleg()
